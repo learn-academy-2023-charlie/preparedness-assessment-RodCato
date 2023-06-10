151e49be598595d3f2a344190d83774b210b97de
RodCato <code@eliment.org>
2023-06-10 13:29:21 -0500

    Functionality complete, just styling left

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,22 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [name, setName] = useState("")
+  const [modalOpen, setModalOpen] = useState(false);
+  const toggleModal = () => {
+    setModalOpen(!modalOpen)
+  }
+  const resetInput = () => {
+    setName("")
+  }
+
   return (
     <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
-      <div className="form">
+    <h1 id="header">Preparedness Assessment</h1>
+      <div className="form" id="center-form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label for="name" id="input-label">Enter your name</Label>
+          <Input type="text" 
+           value={name} 
+           onChange={(e) => setName(e.target.value)} 
+           id="form-input"
+           />
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <Button onClick={toggleModal}>Click Me</Button>
+        <Button type="reset" onClick={resetInput}>
+          Reset
+        </Button>
+        <ModalComponent isOpen={modalOpen} toggle={toggleModal} name={name}/>
       </div>
     </div>
+
   )
 }
 
